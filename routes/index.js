eef921aba7ef22c87140474cbd268ec469ecdb48
Angelo <angelo.lop.alv@gmail.com>
2022-08-11 11:38:12 -0300

    Connected API with categories

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const axios = require("axios");
 const app = express();
 const Sentry = require('@sentry/node'); // Error tracking mandatory, observed in topic 4.8 - Error Tracking, in the learning hub 
 const Tracing = require('@sentry/tracing'); // Tracing required for sentry 
+const secretKey = "$2a$08$p3my8MGizWp3L8f6sn0PCO2c4mLv.mewFcpcfy8pGxHFi0iT4cUX."
 
 Sentry.init({                 // code required for sentry (error tracking)
   dsn: "https://df3e3800fa8648b3bfc0c2e2bf19b72a@o1354822.ingest.sentry.io/6638753",  
@@ -19,13 +20,12 @@ app.use(Sentry.Handlers.tracingHandler()); //Handlers for sentry (error tracking
 app.set("view engine", "pug");
 
 app.get("/", async (req, res) => {   //API from OSF, i am going to make the routes
-  const query = await axios.get("https://backend-academy-osf.herokuapp.com/api/products/product_search", {
+  const query = await axios.get("https://backend-academy-osf.herokuapp.com/api/categories", {
     params: {
-      id: 25565189,
-      secretKey: "$2a$08$p3my8MGizWp3L8f6sn0PCO2c4mLv.mewFcpcfy8pGxHFi0iT4cUX."
+      secretKey: secretKey
     }
   })
-  res.render("index", console.dir(query));
+  res.render("index", { lists: query.data });
 });
 
 app.use(Sentry.Handlers.errorHandler()); //error handler necessary for Sentry
