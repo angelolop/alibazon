f083465d332ab4baef6b498b5f78f15f1500cfca
Angelo <angelo.lop.alv@gmail.com>
2022-08-15 17:39:42 -0300

    All the pages created, functionality of API improved, pages linked

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const Sentry = require('@sentry/node'); // Error tracking mandatory, observed in topic 4.8 - Error Tracking, in the learning hub 
 const Tracing = require('@sentry/tracing'); // Tracing required for sentry 
 const secretKey = "$2a$08$p3my8MGizWp3L8f6sn0PCO2c4mLv.mewFcpcfy8pGxHFi0iT4cUX."
+const apiCategory = "https://backend-academy-osf.herokuapp.com/api/categories/parent/"
 
 Sentry.init({                 // code required for sentry (error tracking)
   dsn: "https://df3e3800fa8648b3bfc0c2e2bf19b72a@o1354822.ingest.sentry.io/6638753",  
@@ -17,34 +18,27 @@ Sentry.init({                 // code required for sentry (error tracking)
 app.use(Sentry.Handlers.requestHandler()); //Handlers for sentry (error tracking)
 app.use(Sentry.Handlers.tracingHandler()); //Handlers for sentry (error tracking)
 app.use(express.static('images'));
+app.use(express.json());
 
 app.set("view engine", "pug");
 
-app.get("/", async (req, res) => {   //Route home, for categories
-  const query = await axios.get("https://backend-academy-osf.herokuapp.com/api/categories", {
+app.get("/gender/:id", async (req, res) => { 
+  let id = req.params.id;  //Route home, for categories
+  const query = await axios.get(apiCategory + id, {// Put men or women depending on the request
     params: {
       secretKey: secretKey
     }
   })
-  res.render("index", { lists: query.data });
-});
+  res.render("categories", { cardsOfCategories: query.data, jumbotron: true});
+}); 
 
-app.get("/subcategories", async (req, res) => {   //Route for subcategories
-  const query = await axios.get("https://backend-academy-osf.herokuapp.com/api/products/product_search?primary_category_id=womens-clothing-tops", {
+app.get("/", async (req, res) => {   //Route for the especified product
+  const query = await axios.get("https://backend-academy-osf.herokuapp.com/api/products/product_search?page=1&primary_category_id=womens-clothing-tops", {
     params: {
       secretKey: secretKey
     }
-  })
-  res.render("index", { lists: query.data });
-});
-
-app.get("/product", async (req, res) => {   //Route for the especified product
-  const query = await axios.get("https://backend-academy-osf.herokuapp.com/api/products/product_search?id=25565189", {
-    params: {
-      secretKey: secretKey
-    }
-  })
-  res.render("index", { lists: query.data });
+  });
+  res.render("products", { cardsOfProducts: query.data, jumbotron: false});
 });
 
 app.use(Sentry.Handlers.errorHandler()); //error handler necessary for Sentry
