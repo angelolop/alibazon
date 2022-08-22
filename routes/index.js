1d8d492ba3806f20acdaaa78b394514f3ba3092c
Angelo <angelo.lop.alv@gmail.com>
2022-08-21 21:27:57 -0300

    add login, logout, register and profile

--- routes/index.js
+++ routes/index.js
@@ -127,11 +127,6 @@ router.get('/logout', function(req, res, next) {
 
 // GET /profile
 router.get('/profile', middle.requiresLogin, function(req, res, next) {
-    if (! req.session.userId ) {
-      var err = new Error("You are not authorized to view this page.");
-      err.status = 403;
-      return next(err);
-    }
     User.findById(req.session.userId)
         .exec(function (error, user) {
           if (error) {
